cf77051cf61837b18f7a003e3f85771ddc8d2e3a
Elie <niyubwayoiraelie5777@gmail.com>
2025-07-24 15:37:10 +0200

    Solved metadata deployment issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,38 +16,53 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Niyubwayo Irakoze Elie | Software Engineer",
-  description: "I'm Niyubwayo Irakoze Elie, a passionate Software Engineer from Rwanda, skilled in building scalable web applications using modern technologies like Next.js, Express.js, TypeScript, and PostgreSQL.",
-};
-
-export const openGraph = {
-  title: "Niyubwayo Irakoze Elie | Software Engineer",
-  description: "I'm Niyubwayo Irakoze Elie, a passionate Software Engineer from Rwanda, skilled in building scalable web applications using modern technologies like Next.js, Express.js, TypeScript, and PostgreSQL.",
-  url: "https://iraelie.vercel.app",
-  siteName: "Niyubwayo Irakoze Elie",
-  images: [
+  authors: [
     {
-      url: "/Elie.jpeg",
-      width: 1200,
-      height: 630,
-      alt: "Niyubwayo Irakoze Elie | Software Engineer",
+      name: "Niyubwayo Irakoze Elie",
+      url: "https://iraelie.vercel.app",
     },
   ],
-};
-// add twitter metadata
-export const twitter = {
-  title: "Niyubwayo Irakoze Elie | Software Engineer",
   description: "I'm Niyubwayo Irakoze Elie, a passionate Software Engineer from Rwanda, skilled in building scalable web applications using modern technologies like Next.js, Express.js, TypeScript, and PostgreSQL.",
-  card: "summary_large_image",
-  site: "@iraelie251006",
-  creator: "@iraelie251006",
-  images: [
-    {
-      url: "/Elie.jpeg",
-      width: 1200,
-      height: 630,
-      alt: "Niyubwayo Irakoze Elie | Software Engineer",
-    },
-  ]
+  keywords: [
+    "Niyubwayo Irakoze Elie",
+    "Software Engineer",
+    "Web Developer",
+    "Next.js",
+    "Express.js",
+    "TypeScript",
+    "PostgreSQL",
+    "Rwanda",
+  ],
+  creator: "Niyubwayo Irakoze Elie",
+  openGraph: {
+    title: "Niyubwayo Irakoze Elie | Software Engineer",
+    description: "I'm Niyubwayo Irakoze Elie, a passionate Software Engineer from Rwanda, skilled in building scalable web applications using modern technologies like Next.js, Express.js, TypeScript, and PostgreSQL.",
+    url: "https://iraelie.vercel.app",
+    siteName: "Niyubwayo Irakoze Elie",
+    images: [
+      {
+        url: "/Elie.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Niyubwayo Irakoze Elie | Software Engineer",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@iraelie251006",
+    creator: "@iraelie251006",
+    title: "Niyubwayo Irakoze Elie | Software Engineer",
+    description: "I'm Niyubwayo Irakoze Elie, a passionate Software Engineer from Rwanda, skilled in building scalable web applications using modern technologies like Next.js, Express.js, TypeScript, and PostgreSQL.",
+    images: [
+      {
+        url: "/Elie.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Niyubwayo Irakoze Elie | Software Engineer",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
