7b592dc2b9c66e3550ecc85af659bd8170f2790a
Elie <niyubwayoiraelie5777@gmail.com>
2025-07-27 23:17:12 +0200

    Updated hero button link from about to projects once clicked on it

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,7 +34,7 @@ const Hero = () => {
                 <p className='text-center md:tracking-wider mb-4 text-[17px] md:text-lg lg:text-2xl'>
                     Hi 👋, I&apos;m Elie, a Software Engineer based in Rwanda 🇷🇼
                 </p>
-                <a href="#about">
+                <a href="#projects">
                     <MagicButton
                      title='Show My Work'
                      icon={<FaLocationArrow />}
