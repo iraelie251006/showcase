ed39599c282c5ef77edf1fcc3a81bb36345fd394
Elie <niyubwayoiraelie5777@gmail.com>
2025-07-29 23:33:46 +0200

    Installed react player and added coming soon section

---
+++ components/ComingSoon.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ComingSoon = () => {
+  return (
+    <section>
+        <h1 className='heading'>
+            Coming
+            <span className='text-purple'> Soon</span>
+        </h1>
+    </section>
+  )
+}
+
+export default ComingSoon
