5a48c8c7c33efd92fdacff6e7701d880c472950d
Elie <niyubwayoiraelie5777@gmail.com>
2025-07-31 18:25:44 +0200

    decreased opacity of 100 on large screen to 50

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -118,14 +118,14 @@ export const BentoGridItem = ({
             <div className="flex gap-1 lg:gap-2 w-fit absolute -right-3 lg:-right-2">
               <div className="flex flex-col gap-3 lg:gap-7">
                 {['Next.js', 'Typescript', 'Postgresql'].map((item) => (
-                  <span key={item} className="py-2 lg:py-5 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>
+                  <span key={item} className="py-2 lg:py-5 lg:px-3 px-3 text-xs lg:text-base opacity-50 rounded-lg text-center bg-[#10132E]">{item}</span>
                 ))}
                 <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
               </div>
               <div className="flex flex-col gap-3 lg:gap-7">
                 <span className="py-4 px-3 rounded-lg text-center bg-[#10132a]"/>
                 {['Python', 'Express.js', 'MongoDB'].map((item) => (
-                  <span key={item} className="py-2 lg:py-5 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>
+                  <span key={item} className="py-2 lg:py-5 lg:px-3 px-3 text-xs lg:text-base opacity-50 rounded-lg text-center bg-[#10132E]">{item}</span>
                 ))}
                 
               </div>
