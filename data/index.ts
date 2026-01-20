1a36be67dd0e69a7e35cc6d0182bb26a65132af4
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-20 22:36:04 +0200

    Changed the name of new profile

--- data/index.ts
+++ data/index.ts
@@ -91,7 +91,7 @@ export const navItems = [
     {
       quote:
         "Collaborating with Elie was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Elie's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
+      name: "Harerimana Thierry",
       image: "/profile.svg",
       title: "Director of AlphaStream Technologies",
     },
