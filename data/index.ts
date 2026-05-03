66e38718d9490cf7a7215e1d4243c00f707272c5
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-03 19:26:16 +0200

    Remove unused React import in Hero component and update grid item title for clarity

--- data/index.ts
+++ data/index.ts
@@ -8,7 +8,7 @@ export const navItems = [
   export const gridItems = [
     {
       id: 1,
-      title: "I prioritize client collaboration, fostering open communication ",
+      title: "I thrive in collaborative engineering environments, with a focus on clean code and clear communication.",
       description: "",
       className: "lg:col-span-2 md:col-span-6 md:row-span-2 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
